ee8225cdf11dd4e70e6536e76245aa1aae3027f7
Staz1x <fogel.stazix@gmail.com>
2023-08-31 08:11:10 +0200

    First

---
+++ ItSec2.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class ItSec2 {
+
+    public static void main(String[] args) {
+
+        String password = "gers";
+        String encryptedPassword = encrypt(password);
+
+        //int tries = decrypter(password, encryptedPassword);
+
+        System.out.println("Password = "+ password);
+        System.out.println("Encrypted password = " + encryptedPassword);
+        //System.out.println("It took " + tries + " tries to find");
+        System.out.println(decrypter(password, encryptedPassword));
+
+    }
+
+    public static String encrypt(String password) {
+        List<Character> letters = new ArrayList<>();
+        for(char c = 'A'; c <= 'Z'; c++) {
+            letters.add(c);
+        }
+        for(char c = 'a'; c <= 'z'; c++){
+            letters.add(c);
+        }
+
+        Collections.shuffle(letters, new Random());
+        StringBuilder encrypted = new StringBuilder();
+
+        for(char letter : password.toCharArray()) {
+            char encryptedLetter = letter;
+            if(Character.isLetter(letter)) {
+                int index = Character.isUpperCase(letter) ? letter - 'A' : letter - 'a';
+                encryptedLetter = letters.get(index);
+            }
+            encrypted.append(encryptedLetter);
+        }
+        return encrypted.toString();
+    }
+
+
+    // Add timer for fun
+    public static String decrypter(String password, String encryptedPassword) {
+        List<Character> letters = new ArrayList<>();
+        for (char c = 'A'; c <= 'Z'; c++) {
+            letters.add(c);
+        }
+        for (char c = 'a'; c <= 'z'; c++) {
+            letters.add(c);
+        }
+
+        int tries = 0;
+        StringBuilder pass = new StringBuilder(encryptedPassword);
+        while (!pass.toString().equals(password)) {
+            Collections.shuffle(letters, new Random());
+            for (int i = 0; i < password.length(); i++) {
+                char originalLetter = password.charAt(i);
+                int index = Character.isUpperCase(originalLetter) ? originalLetter - 'A' : originalLetter - 'a';
+                char encryptedLetter = letters.get(index);
+                pass.setCharAt(i, encryptedLetter);
+            }
+            tries++;
+        }
+
+        return "It took " + tries + " tries to find it";
+    }
+
+}
